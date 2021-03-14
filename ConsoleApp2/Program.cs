156b366f4900baa362b1ce7363b8af19ce9bd4eb
beyzaatar50 <beyzaatar50@gmail.com>
2021-03-14 14:16:21 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using Dictionary;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> kullanicilar = new MyDictionary<int, string>();
+            kullanicilar.Add(0, "Kerem ÖZER");
+            kullanicilar.Add(1, "Ali RODOPLU");
+            kullanicilar.Add(2, "Okan BİLİR");
+
+            kullanicilar.Listele();
+        }
+    }
+}
